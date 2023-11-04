9686281f1ab18e14ebf3d14793cb1bec0480bb5d
thirteenpylons <thirteenpylons@gmail.com>
2023-11-03 21:40:27 -0400

    completed exact change

--- scripting/week_three/exact_change.py
+++ scripting/week_three/exact_change.py
@@ -1,5 +1,8 @@
 """
-Write a program with total change amount as an integer input, and output the change using the fewest coins, one coin type per line. The coin types are Dollars, Quarters, Dimes, Nickels, and Pennies. Use singular and plural coin names as appropriate, like 1 Penny vs. 2 Pennies.
+Write a program with total change amount as an integer input, 
+and output the change using the fewest coins, one coin type per line. 
+The coin types are Dollars, Quarters, Dimes, Nickels, and Pennies. 
+Use singular and plural coin names as appropriate, like 1 Penny vs. 2 Pennies.
 
 Ex: If the input is:
 
@@ -16,8 +19,36 @@ the output is:
 2 Dimes 
 """
 
+
+def calculate_change(change_amount: int) -> str:
+    coins = {
+        100: ("Dollar", "Dollars"),
+        25: ("Quarter", "Quarters"),
+        10: ("Dime", "Dimes"),
+        5: ("Nickel", "Nickels"),
+        1: ("Penny", "Pennies")
+    }
+    
+    output = []
+
+    # Calculate the number of each coin type
+    for value, names in coins.items():
+        num_coins = change_amount // value
+        if num_coins == 1:
+            output.append(f"{num_coins} {names[0]}")
+        elif num_coins > 1:
+            output.append(f"{num_coins} {names[1]}")
+        change_amount %= value
+
+    return "\n".join(output)
+
 def main():
-    pass
+    change_amount = int(input())
+    
+    if change_amount <= 0:
+        print("No change")
+    else:
+        print(calculate_change(change_amount))
 
 
 if __name__ == "__main__":
