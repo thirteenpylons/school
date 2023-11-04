353cc2526910420c132d1800fa8bd97639ccd6e4
thirteenpylons <thirteenpylons@gmail.com>
2023-11-03 21:50:02 -0400

    init week four work... have to try and stay ahead

---
+++ scripting/week_four/count_input_len.py
@@ -0,0 +1,19 @@
+"""
+Given a line of text as input, output the number of characters excluding spaces, periods, or commas.
+
+Ex: If the input is:
+
+Listen, Mr. Jones, calm down.
+the output is:
+
+21
+Note: Account for all characters that aren't spaces, periods, or commas (Ex: "r", "2", "!").
+
+"""
+
+def main():
+    user_text = input()
+
+
+if __name__ == "__main__":
+    main()
--- scripting/week_three/seasons.py
+++ scripting/week_three/seasons.py
@@ -27,13 +27,48 @@ Winter: December 21 - March 19
 
 """
 
+def determine_season(month: str, day: int) -> str:
+    """
+    Determine the season based on month and day.
+    """
+    # Define a dictionary with month as key and (start day, end day, season) as values
+    seasons = {
+        'January': (1, 31, 'Winter'),
+        'February': (1, 29, 'Winter'),  # 29 to account for leap years, but additional check needed
+        'March': (1, 19, 'Winter'),
+        'March': (20, 31, 'Spring'),
+        'April': (1, 30, 'Spring'),
+        'May': (1, 31, 'Spring'),
+        'June': (1, 20, 'Spring'),
+        'June': (21, 30, 'Summer'),
+        'July': (1, 31, 'Summer'),
+        'August': (1, 31, 'Summer'),
+        'September': (1, 21, 'Summer'),
+        'September': (22, 30, 'Autumn'),
+        'October': (1, 31, 'Autumn'),
+        'November': (1, 30, 'Autumn'),
+        'December': (1, 20, 'Autumn'),
+        'December': (21, 31, 'Winter')
+    }
+    
+    # Check for February in leap years
+    if month == 'February' and day == 29:
+        if not (year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)):
+            return 'Invalid'
+
+    # Check if month is valid
+    if month in seasons:
+        start_day, end_day, season = seasons[month]
+        if start_day <= day <= end_day:
+            return season
+
+    return 'Invalid'
+
 def main():
-    input_month = input()
-    input_day = int(input())
+    month = input()
+    day = int(input())
 
-    ''' Type your code here. '''
-
-
+    print(determine_season(month, day))
 
 if __name__ == "__main__":
     main()
