7a0648071e45a57f5d2828c099115038c02f0fc9
thirteenpylons <thirteenpylons@gmail.com>
2023-12-05 14:36:58 -0500

    completed word_frequencies_lists

--- scripting/week_seven/word_frequencies_lists.py
+++ scripting/week_seven/word_frequencies_lists.py
@@ -25,43 +25,27 @@ Note: There is a newline at the end of the output, and input1.csv
 is available to download.
 
 """
-import csv
-
-
 def read_csv(file_name: str) -> list:
-    """
-    Returns the contents read from the CSV file filename as list.
-
-    This function reads the contents of the file filename and returns the contents as
-    a 2-dimensional list. Each element of the list is a row, with the first row being
-    the header. Cells in each row are all interpreted as strings; it is up to the 
-    programmer to interpret this data, since CSV files contain no type information.
-
-    Parameter filename: The file to read
-    Precondition: filename is a string, referring to a file that exists, and that file 
-    is a valid CSV file
-    """
-    with open(file_name) as f:
-        wrap = csv.reader(f)
-        data = list(wrap)
+    with open(file_name, 'r') as this_file:
+        data = this_file.readline().split(',')
     return data
 
-def count_words(sentence: list) -> list:
+def count_words(words: list) -> dict:
     counted_words = {}
-    for top_list in sentence:
-        for word in top_list:
-            counted_words.update(word += 1)
+    for word in words:
+        word = word.strip()  # Strip whitespace
+        if word in counted_words:
+            counted_words[word] += 1
+        else:
+            counted_words[word] = 1
     return counted_words
 
 def main():
-    #file_name = input("Enter the name of the file: ")
-    file_name = "./input1.csv"
-    list_from_csv: list = read_csv(file_name)
-    print(list_from_csv)
-    result = count_words(list_from_csv)
-    print(result)
-
-
+    file_name = input() # read the name of the input file
+    words_from_csv = read_csv(file_name)
+    result = count_words(words_from_csv)
+    for word, count in result.items():
+        print(f"{word} {count}")
 
 if __name__ == "__main__":
     main()
